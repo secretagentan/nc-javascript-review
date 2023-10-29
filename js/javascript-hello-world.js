9c55642872ddfb0a89b29b59f29a86d0e71ed319
an <anthedev@gmail.com>
2023-10-28 22:35:14 -0700

    first commit

---
+++ js/javascript-hello-world.js
@@ -0,0 +1,11 @@
+// alert('Hello World!');
+function sayHello() {
+    alert('Hello World!');
+    console.log('Alert sent!')
+}
+sayHello();
+
+function getName() {
+    const userName = prompt('What is your name?');
+    alert('Hello ' + userName + '!');
+}
