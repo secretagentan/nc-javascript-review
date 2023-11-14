1106b93ec4e1c3b83fd3a85cb55173e76238087c
an <anthedev@gmail.com>
2023-11-14 04:35:11 -0800

    [wip] workshop 3 - task 3

--- js/registration.js
+++ js/registration.js
@@ -14,27 +14,31 @@ class Bootcamp {
 
     registerStudent(studentToRegister) {
         if (!studentToRegister.name || !studentToRegister.email) {
-            console.log('Invalid name or email');
+            console.log('Invalid name or email.');
             return false;
-        } else {
-            this.students.find((studentToRegister) => {
-                if (true) {
-                    console.log(`The email address ${studentToRegister.email} is already registered.`)
-                    return false;
-                }
-            })
-            this.students.push(studentToRegister);
-            // console.log(studentToRegister);
-            console.log(`Registering ${studentToRegister.name} to the ${this.name} bootcamp.`);
-            return true;
         }
+
+        const isRegistered = this.students.find(student => student.email === studentToRegister.email);
+
+        if (isRegistered) {
+            console.log(`The email address ${studentToRegister.email} is already registered.`)
+            return false;
+        }
+
+        this.students.push(studentToRegister);
+        console.log(`Registering ${studentToRegister.name} to the ${this.name} bootcamp.`);
+        return true;
     }
+
+    // listStudents() {
+    //     if (this.students.length === 0)
+    //         console.log(`No students are registered to the ${this.name} bootcamp.`);
+    //     return false;
+    // }
 }
 
 
-
-
-// testing task 1
+// task 1 test
 testStudent = new Student('Bugs Bunny', 'bugs@bunny.com');
 console.log(testStudent);
 
@@ -42,7 +46,7 @@ if (testStudent.name === 'Bugs Bunny' && testStudent.email === 'bugs@bunny.com')
     console.log('TASK 1: PASS');
 }
 
-// testing task 2
+// task 2 test
 reactBootcamp = new Bootcamp("React", "Advanced");
 console.log(reactBootcamp);
 
@@ -50,7 +54,7 @@ if (reactBootcamp.name === 'React' && reactBootcamp.level === 'Advanced' && Arra
     console.log('TASK 2: PASS');
 }
 
-// testing task 3
+// task 3 test
 const runTest = (bootcamp, student) => {
     const attemptOne = bootcamp.registerStudent(student);
     const attemptTwo = bootcamp.registerStudent(student);
